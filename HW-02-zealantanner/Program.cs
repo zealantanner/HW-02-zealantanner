e01050785b01d3bd40a7f998024264a14ef46f73
zealantanner <77339919+zealantanner@users.noreply.github.com>
2021-01-26 16:44:13 -0700

    Add files via upload

---
+++ HW-02-zealantanner/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace CS1405_Lab
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hi");
+
+            string a = "";
+            string b = null;
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+
+            Console.WriteLine("\"\" {0} \"\"", a == "" ? "equals": "does not equal");
+            Console.WriteLine("\"\" {0} null", a == null ? "equals": "does not equal");
+
+            Console.WriteLine("null {0} \"\"", b == "" ? "equals": "does not equal");
+            Console.WriteLine("null {0} null", b == null ? "equals": "does not equal");
+
+            int? i = null + 1;
+            Console.WriteLine($"\"{i}\"");
+        }
+    }
+}
